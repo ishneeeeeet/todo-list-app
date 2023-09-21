80e20ed0ff709bb930e454845dd772c7b3ee73e5
ishneeeeeet <ishneet100@gmail.com>
2023-09-20 21:12:22 -0600

    new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.css 	new file:   src/App.jsx 	new file:   src/assets/react.svg 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  useEffect(() => {
+    const fetchTodo = async () => {
+      const response = await axios.get("http://localhost:1337/api/todos");
+      setTodoList(response.data.data);
+      console.log(response.data.data);
+    };
+    fetchTodo();
+  }, []);
+
+  const handleAddTodo = async () => {
+    try {
+      const response = await axios.post("http://localhost:1337/api/todos", {
+        data: {
+          description: todo,
+        },
+      });
+      console.log(response.data);
+      setTodoList([...todoList, response.data.data]);
+      setTodo("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    const response = await axios.delete(
+      `http://localhost:1337/api/todos/${id}`
+    );
+    console.log(response.data);
+  };
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button onClick={handleAddTodo}>Add Todo</button>
+      </div>
+      <div>
+        <ul>
+          {todoList.map((todo) => (
+            <li key={todo.id}>
+              {todo.attributes.description}{" "}
+              <button onClick={() => handleDelete(todo.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
